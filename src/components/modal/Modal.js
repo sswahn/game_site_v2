7c94b340b18d6433862ae39e80f8369d0dde7c8b
sswahn <36422765+sswahn@users.noreply.github.com>
2021-07-21 20:06:49 -0400

    initial commit

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,38 @@
+import { useContext } from 'react'
+import { Context } from '../Provider'
+import Login from './Login'
+import Cart from './Cart'
+
+export default function Modal() {
+  const [context, dispatch] = useContext(Context)
+
+  const close = () => {
+    dispatch({ type: 'modal' })
+  }
+ 
+  const renderContent = () => {
+    switch(context.modal) {
+      case 'login':
+        return <Login />
+      case 'cart':
+        return <Cart />
+      default:
+        return
+    }
+  }
+
+  return (
+    <div id="modal" className={styles.modal} style={{
+      display: context.modal ? 'block' : 'none'
+    }}>
+      <div>
+        <button className={styles.button} onClick={close}>
+          {/* fa times icon */}
+        </button>
+      </div>
+      <div id="content" className={styles.content}>
+        {renderContent()}
+      </div>
+    </div>
+  )
+}
