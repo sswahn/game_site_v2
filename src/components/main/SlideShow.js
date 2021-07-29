20b5c051302d2e7a02ea1ca5d1b98eee7f02cfe4
sswahn <36422765+sswahn@users.noreply.github.com>
2021-07-29 12:09:55 -0400

    tooltip slideshows working

--- src/components/main/SlideShow.js
+++ src/components/main/SlideShow.js
@@ -1,46 +1,23 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './main.module.css'
 
 export default ({ images, isHovering }) => {
   const [state, setState] = useState({ index: 0 })
-  const is_mounted = useRef(true)
-
-  const slideShow = async () => {
-    if (!isHovering && is_mounted.current) {
-      return
-    }
-    const slides = document.getElementById('slideshow').children
-    if (state.index !== 0) {
-      slides[state.index - 1].style.display = 'none'
-    }
-    if (state.index > slides.length - 1) {
-      slides[0].style.display = 'block'
-      return setState({ ...state, index: 0 })
-    }
-    slides[state.index].style.display = 'block'
-    await new Promise(resolve => setTimeout(resolve, 2500))
-    setState({ ...state, index: state.index + 1 })
-  }
 
   useEffect(() => {
-    slideShow()
-    return () => {
-      is_mounted.current = false
-    }
+    setTimeout(() => setState({ index: state.index === images.length - 1 ? 0 : state.index + 1 }), 2500)
+    return () => {}
   }, [state.index])
 
-  // problem is loop below and looping above, why two loops? dunno.
-  // still think the problem is useEffect, shouldnt use that to replace loops
-  // cause it causes re-renders! 
-
   return (
-    <div id="slideshow" className={styles.slideshow}>
-      {images.map((image, index) => {
-        console.log(index, image)
-        return <div key={index} className={`${styles.slides} ${styles.fade}`} style={index === 0 ? {display:'block'} : {}}>
-          <img src={image} alt={index} />
-        </div>
-      })}
+    <div className={styles.slideshowcontainer}>
+      <div id="slideshow" className={styles.slideshow}>
+        {images.map((image, index) =>
+          <div key={index} style={{ transform: `translate3d(${-state.index * 100}%, 0, 0)` }}>
+            <img src={image} alt={index} />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
